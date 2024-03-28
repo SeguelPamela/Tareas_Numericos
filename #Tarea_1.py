8afa1c968528c3d6c52585402ecf1d6bbb0c80bf
SeguelPamela <pamela.seguel@ug.uchile.cl>
2024-03-28 01:30:16 -0300

    Todo ok menos la p5 y modificar gráficos

--- #Tarea_1.py
+++ #Tarea_1.py
@@ -5,16 +5,41 @@ import matplotlib.pyplot as plt
 
 #P1)
 #Valores de h y x
-x = np.linspace((1-0.5610), (1+0.5610), 100).reshape((100,1))
-h = np.logspace((10^-8), (10^-1), 18, base=10).reshape((1,18))
-#Valores de la derivada de las funciones dadas
-sen_x = (-np.sin(x+2*h)+ 8*np.sin(x+h)-8*np.sin(x-h)+np.sin(x-2*h))/(12*h)
-log_x = (-np.log(x+2*h)+8*np.log(x+h)-8*np.log(x-h)+np.log(x-2*h))/(12*h)
+x = np.linspace((1-0.5610), (1+0.5610), 100).reshape((1,100))
+h = np.logspace((10^-8), (10^-1), 18, base=10).reshape((18,1))
+
+#Valores de la derivada según el enunciado de las funciones dadas
+der_sen_x = (-np.sin(x+2*h)+ 8*np.sin(x+h)-8*np.sin(x-h)+np.sin(x-2*h))/(12*h)
+der_log_x = (-np.log(x+2*h)+8*np.log(x+h)-8*np.log(x-h)+np.log(x-2*h))/(12*h)
 
 #P2)
-#Para ambas funciones clacule la diferencia entre los valores
-#de la derivada estimados en la parte anterior con el valor real de la derivada
-#CUANDO HABLA DE LA DERIVADA REAL SE REFIERE A LA DERIVADA Q VIMOS EN CLASES O A LA DERIVADA OFICIAL ONDA NP.DERIVADA O ALGO ASI???
-#No hay q usar otra formulita
-#Para la del seno es coseno
-#Para la del log es 1/(xln(10))
+#Primero se calcula el valor de la derivada real
+der_sen_real = (np.sin(x+h) - np.sin(x-h))/(2*h)
+der_log_real = (np.log(x+h) - np.log(x-h))/(2*h)
+#Luego se calcula la diferencia entre estos valores
+dif_sen = der_sen_x - der_sen_real
+dif_log = der_log_x - der_log_real
+
+#P3)
+#Se grafican las curvas de las diferencias calculadas en la parte 2
+#graficamos para la diferencia de las derivadas del seno
+#MODIFICAR LOS GRÁFICOS
+plt.plot(np.fabs(dif_sen))
+plt.xscale('log')
+#graficamos para la diferencia de las derivadas del log
+plt.plot(np.fabs(dif_log))
+plt.xscale('log')
+
+
+#P4)
+#Generamos la curva promedio para cada gráfico
+
+#Primero la del Seno
+A = np.fabs(dif_sen.mean(1))
+plt.plot(A, linewidth = 10)
+
+#Luego la del Coseno
+B = np.fabs(dif_log.mean(1))
+plt.plot(B, linewidth = 10)
+
+#P5) REPETIR EL GRAFICO CON NUMEROS DEL TIPO NP.FLOAT32
